6622469e1620185f41867fecc3d7019de36a15fa
hamaguchi-tescom <y-hamaguchi@tescomsound.com>
2021-07-19 17:01:56 +0900

    ディレクトリ整理

--- kind2_db_test.py
+++ kind2_db_test.py
@@ -9,9 +9,12 @@ time = sys.argv[1]
 conn = sqlite3.connect(db_path)
 # print("データベースに接続しました。")
 # sql = "SELECT * FROM recognition_record WHERE STRFTIME('%Y-%m-%d', add_time) = STRFTIME('%Y-%m-%d', 'NOW')"
-sql = "SELECT add_time, work_number, recognition_name,temperature_val FROM recognition_record_view where add_time like ?"
-for row in conn.execute(sql,(time+'%',)):
+sql = "SELECT add_time, equipment_name, work_number, recognition_name,temperature_val FROM recognition_record_view where add_time like ?"
+with open(f'{time}.csv','a',newline='',encoding='utf_8_sig') as csvfile:
+	c = csv.writer(csvfile,delimiter=',')
+	c.writerow(['add_time','equipment_name','work_number','recognition_name','temperature_val'])
+	for row in conn.execute(sql,(time+'%',)):
 		print(row)
-		
+		c.writerow(row)
 	
 conn.close()
