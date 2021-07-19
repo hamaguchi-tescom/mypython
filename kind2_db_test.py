f0309e4608dfb6a11d09d904694fb2b2bef73287
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-07-19 18:10:41 +0900

    環境設定

--- kind2_db_test.py
+++ kind2_db_test.py
@@ -3,18 +3,19 @@ import csv
 import sys
 import sqlite3
 
-db_path = "C:\ProgramData\ArcSoftDoorSingle\doorsingleKD.db"
+db_path = "./doorsingleKD.db"
 time = sys.argv[1]
 
 conn = sqlite3.connect(db_path)
 # print("データベースに接続しました。")
 # sql = "SELECT * FROM recognition_record WHERE STRFTIME('%Y-%m-%d', add_time) = STRFTIME('%Y-%m-%d', 'NOW')"
 sql = "SELECT add_time, equipment_name, work_number, recognition_name,temperature_val FROM recognition_record_view where add_time like ?"
-with open(f'{time}.csv','a',newline='',encoding='utf_8_sig') as csvfile:
-	c = csv.writer(csvfile,delimiter=',')
-	c.writerow(['add_time','equipment_name','work_number','recognition_name','temperature_val'])
-	for row in conn.execute(sql,(time+'%',)):
-		print(row)
-		c.writerow(row)
-	
+with open(f'{time}.csv', 'a', newline='', encoding='utf_8_sig') as csvfile:
+    c = csv.writer(csvfile, delimiter=',')
+    c.writerow(['add_time', 'equipment_name', 'work_number',
+                'recognition_name', 'temperature_val'])
+    for row in conn.execute(sql, (time+'%',)):
+        print(row)
+        c.writerow(row)
+
 conn.close()
