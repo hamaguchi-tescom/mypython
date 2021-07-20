bb3a020ac24ef02ef7bbbc4db8ec5e0956a648d2
yoheihamaguchi <yoheihamaguchi251@gmail.com>
2021-07-20 20:05:05 +0900

    実験

--- kind2_db_test.py
+++ kind2_db_test.py
@@ -1,21 +1,52 @@
 ﻿# coding: utf-8
-import csv
+# import csv
 import sys
 import sqlite3
+from openpyxl import Workbook
+from openpyxl.styles import numbers, PatternFill
+from openpyxl.utils import get_column_letter
+
 
 db_path = "./doorsingleKD.db"
 time = sys.argv[1]
 
 conn = sqlite3.connect(db_path)
-# print("データベースに接続しました。")
 # sql = "SELECT * FROM recognition_record WHERE STRFTIME('%Y-%m-%d', add_time) = STRFTIME('%Y-%m-%d', 'NOW')"
-sql = "SELECT add_time, equipment_name, work_number, recognition_name,temperature_val FROM recognition_record_view where add_time like ?"
-with open(f'{time}.csv', 'a', newline='', encoding='utf_8_sig') as csvfile:
-    c = csv.writer(csvfile, delimiter=',')
-    c.writerow(['add_time', 'equipment_name', 'work_number',
-                'recognition_name', 'temperature_val'])
-    for row in conn.execute(sql, (time+'%',)):
-        print(row)
-        c.writerow(row)
+sql = "SELECT add_time, equipment_name, work_number, recognition_name,temperature_val FROM recognition_record_view where add_time like ? order by add_time"
 
-conn.close()
+# 書き込み用ファイルを作成
+FILL_TITLE = PatternFill(patternType='solid', fgColor='B0E0E6')
+wb = Workbook()
+ws = wb.active
+y = 1
+ws.cell(row=y, column=1).value = "add_time"
+ws.cell(row=y, column=2).value = "equipment_name"
+ws.cell(row=y, column=3).value = "work_number"
+ws.cell(row=y, column=4).value = "recognition_name"
+ws.cell(row=y, column=5).value = "temperature_val"
+
+for rows in ws['A1':'E1']:
+    for cell in rows:
+        cell.fill = FILL_TITLE
+        value_len = len(str(cell.value)) * 2
+        ws.column_dimensions[get_column_letter(cell.column)].width = value_len
+        print(value_len)
+# ws.column_dimensions['A'].width = 20
+# ws.row_dimensions[1].height = 50
+for row in conn.execute(sql, (time+'%',)):
+    y += 1
+    ws.cell(row=y, column=1).value = row[0]
+    ws.cell(row=y, column=2).value = row[1]
+    ws.cell(row=y, column=3).value = row[2]
+    ws.cell(row=y, column=4).value = row[3]
+    ws.cell(row=y, column=5).value = row[4]
+
+    wb.save(f'{time}.xlsx')
+
+    # with open(f'{time}.csv', 'a', newline='', encoding='utf_8_sig') as csvfile:
+    # c = csv.writer(csvfile, delimiter=',')
+    # c.writerow(['add_time', 'equipment_name', 'work_number',
+    #             'recognition_name', 'temperature_val'])
+    # for row in conn.execute(sql, (time+'%',)):
+    #     print(row)
+    #     c.writerow(row)
--- test.py
+++ test.py
@@ -5,16 +5,30 @@ import datetime
 import codecs
 import sys
 from pathlib import Path
+from openpyxl import Workbook
+import openpyxl
 
 
 # # プログラム実行時の年月日を取得
 dt = datetime.datetime.now()
 date = dt.strftime("%Y-%m-%d")
+# isdate = date.isnumeric
+# # CSVファイルの存在確認　なければ作成
+# logfile = Path('./log/' + date + '.csv')
+# logfile.touch(exist_ok=True)
+# print(logfile)
 
-# CSVファイルの存在確認　なければ作成
-logfile = Path('./log/' + date + '.csv')
-logfile.touch(exist_ok=True)
-print(logfile)
+# wb = Workbook()
+# ws1 = wb.create_sheet('NewSheet1', 0)
+# ws2 = wb.create_sheet('addSheet2', 1)
+# ws1.sheet_properties.tabColor = "1072BA"
+# wb.save('Sample.xlsx')
+path = 'Sample.xlsx'
+wb = openpyxl.load_workbook(path)
+ws = wb.active
+ws['A1'] = 'this is test1'
+ws.title = "test_sheet"
+wb.save('Sample.xlsx')
 
 
 # # ディレクトリ名を年月日にする
@@ -22,7 +36,7 @@ print(logfile)
 # # print(DATE_DIR)
 # if os.path.isdir(DATE_DIR) == False :
 #     # print('make new directory')
-#     os.mkdir(DATE_DIR) 
+#     os.mkdir(DATE_DIR)
 # # else:
 # #     print('directory already exist')
 # print(f'getpreferredencoding: {locale.getpreferredencoding()}')
